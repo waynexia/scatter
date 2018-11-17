eb466f93b52ea7708115cff2beedbe3b017885f4
wayne <i@waynest.com>
2018-11-17 11:08:16 +0800

    version: 0.0.1

---
+++ main.js
@@ -0,0 +1,42 @@
+const {app, BrowserWindow,Menu} = require('electron')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 1500, height: 900})
+
+  mainWindow.loadFile('index.html')
+
+  //mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+let template = [{
+  label: 'About',
+  submenu: [{
+    label:'Author: Waynest',
+    click: () => {}
+  }]
+}]
+
+app.on('ready', ()=>{
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  createWindow()
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
